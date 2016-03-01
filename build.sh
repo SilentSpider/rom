d41bce412e3eccfc2f924854fe6a0c411f2903c6
Johan Lundin <johan@bluenine.se>
2016-03-01 19:31:26 +0000

    using 7z

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ echo "Building ROM"
 rm -f update.zip
 
 cd bootanimation
-7z a ../bootanimation.zip -r *
+7z a -tzip -mx=0 ../bootanimation.zip -r *
 cd ..
 mv bootanimation.zip src/common
 
 cd src
-7z a ../update.zip -r *
+7z a -tzip ../update.zip -r *
