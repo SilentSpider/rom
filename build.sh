b4e50163762d44230ccd28b495dfa64bcae91565
SilentSpider <support@silentspider.com>
2016-03-02 17:46:15 +0000

    Adding twrp and tor installer

--- build.sh
+++ build.sh
@@ -6,15 +6,23 @@
 echo "Building ROM"
 rm -f update.zip
 
+# Build boot animation
 cd bootanimation
 7z a -tzip -mx=0 ../bootanimation.zip -r *
 cd ..
 mv bootanimation.zip src/common
 
+# Build initial boot logo
 cd bootlogo
 python aml-imgpack.py --pack logo.img bootup.bmp upgrade_error.bmp upgrade_fail.bmp upgrade_logo.bmp upgrade_success.bmp upgrade_unfocus.bmp upgrade_upgrading.bmp
 cd ..
 mv bootlogo/logo.img src/common 
 
+# Add TOR artifacts
+rm -fr src/common/tor
+mkdir src/common/tor
+cp build/built/* src/common/tor
+
+# Build OTA zip
 cd src
 7z a -tzip ../update.zip -r *
