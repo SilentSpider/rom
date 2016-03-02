813511fa4f042df99a587d9c9f9f4e299d376967
SilentSpider <support@silentspider.com>
2016-03-02 10:40:57 +0000

    Adding custom boot logo

--- build.sh
+++ build.sh
@@ -11,5 +11,10 @@ cd bootanimation
 cd ..
 mv bootanimation.zip src/common
 
+cd bootlogo
+python aml-imgpack.py --pack logo.img bootup.bmp upgrade_error.bmp upgrade_fail.bmp upgrade_logo.bmp upgrade_success.bmp upgrade_unfocus.bmp upgrade_upgrading.bmp
+cd ..
+mv bootlogo/logo.img src/common 
+
 cd src
 7z a -tzip ../update.zip -r *
